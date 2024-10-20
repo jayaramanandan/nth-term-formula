d004ed1ccb867f6e10d9ad5001fdc5dd5e58468c
Jayaram Anandan <68646971+jayaramanandan@users.noreply.github.com>
2024-10-20 15:44:04 +0000

    worked out nth-terms of each matrix

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from decimal import *
 import math
 from mpmath import mp, matrix, inverse
 
-mp.prec = 200
+mp.prec = 400
+#mp.pretty = True
 
 def create_matrix_of_exponents(power):
     matrix_array = []
@@ -26,6 +27,20 @@ def create_matrix_of_pascal(power):
 def nth_term_of(series):
     return inverse(create_matrix_of_exponents(len(series) - 1)) * matrix(series)
 
+def to_list_matrix1D(matrix_data, layer_n):
+    matrix_array = []
+    for c in range(len(matrix_data)):
+        matrix_array.append(matrix_data[layer_n, c])
+        
+    return matrix_array
+
+def get_nth_term_of_top_layer(matrix_data):
+    row_array = []
+    for c in range(len(matrix_data) - 1):
+        row_array.append(mp.fabs(matrix_data[1, c]))
+    
+    return nth_term_of(matrix(row_array))
+
 
 
 def get_layer(data, layer_n):
@@ -37,14 +52,26 @@ def get_layer(data, layer_n):
 
 
 def main(max_power):
+    nth_term_parts = []
+    
     for p in range(1, max_power):
+        
         inverse_exponent_matrix = inverse(create_matrix_of_exponents(p)) * mp.factorial(p)
         
-        print(inverse_exponent_matrix * create_matrix_of_pascal(p))
+        modified_matrix = inverse_exponent_matrix * create_matrix_of_pascal(p)
+        
+        nth_term_matrix = get_nth_term_of_top_layer(modified_matrix)
+        
+        nth_term_parts.append(mp.fabs(nth_term_matrix[p-5, 0]))
+        
         print("\n\n")
         
-        
-        
-main(10)
+    print(matrix(nth_term_parts))
 
-print(nth_term_of([85, 26, 19, 14, 11]))
+# multiplied by power factorial and divided by pascals triangle terms 
+        
+main(20)
+
+print("\n\n")
+
+print(nth_term_of(matrix([274, 1624, 6769, 22449, 63273, 157773, 357423, 749463, 1474473, 2749747, 4899622])) * mp.factorial(8))   
